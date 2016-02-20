0ca26f8e336e6eb69cd28f98aa1fc97fb6b71741
Tooru Okano <transparency1979@gmail.com>
2016-02-20 19:33:43 +0900

    add sardata definitions.

--- import_sardata/sardata_importer.rb
+++ import_sardata/sardata_importer.rb
@@ -190,6 +190,236 @@ module SardataImporter
           "tcp-tw"  => { :col_name => "tcp_tw",  :data_type => :long },
         }
       },
+      {
+        :type => "net_ip", :fields => {
+          "irec/s"    => { :col_name => "irecps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fwddgm/s"  => { :col_name => "fwddgmps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idel/s"    => { :col_name => "idelps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "orq/s"     => { :col_name => "orqps"    , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmrq/s"   => { :col_name => "asmrqps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmok/s"   => { :col_name => "asmokps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragok/s"  => { :col_name => "fragokps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragcrt/s" => { :col_name => "fragcrtps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_eip", :fields => {
+          "ihdrerr/s" => { :col_name => "ihdrerrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iadrerr/s" => { :col_name => "iadrerrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iukwnpr/s" => { :col_name => "iukwnprps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idisc/s"   => { :col_name => "idiscps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odisc/s"   => { :col_name => "odiscps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "onort/s"   => { :col_name => "onortps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmf/s"    => { :col_name => "asmfps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragf/s"   => { :col_name => "fragfps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_icmp", :fields => {
+          "imsg/s"    => { :col_name => "imsgps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "omsg/s"    => { :col_name => "omsgps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iech/s"    => { :col_name => "iechps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iechr/s"   => { :col_name => "iechrps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oech/s"    => { :col_name => "oechps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oechr/s"   => { :col_name => "oechrps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "itm/s"     => { :col_name => "itmps"    , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "itmr/s"    => { :col_name => "itmrps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "otm/s"     => { :col_name => "otmps"    , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "otmr/s"    => { :col_name => "otmrps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iadrmk/s"  => { :col_name => "iadrmkps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iadrmkr/s" => { :col_name => "iadrmkrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oadrmk/s"  => { :col_name => "oadrmkps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oadrmkr/s" => { :col_name => "oadrmkrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_eicmp", :fields => {
+          "ierr/s"    => { :col_name => "ierrps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oerr/s"    => { :col_name => "oerrps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idstunr/s" => { :col_name => "idstunrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odstunr/s" => { :col_name => "odstunrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "itmex/s"   => { :col_name => "itmexps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "otmex/s"   => { :col_name => "otmexps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iparmpb/s" => { :col_name => "iparmpbps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oparmpb/s" => { :col_name => "oparmpbps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "isrcq/s"   => { :col_name => "isrcqps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "osrcq/s"   => { :col_name => "osrcqps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iredir/s"  => { :col_name => "iredirps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oredir/s"  => { :col_name => "oredirps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_tcp", :fields => {
+          "active/s"  => { :col_name => "activeps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "passive/s" => { :col_name => "passiveps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iseg/s"    => { :col_name => "isegps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oseg/s"    => { :col_name => "osegps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_etcp", :fields => {
+          "atmptf/s"  => { :col_name => "atmptfps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "estres/s"  => { :col_name => "estresps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "retrans/s" => { :col_name => "retransps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "isegerr/s" => { :col_name => "isegerrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "orsts/s"   => { :col_name => "orstsps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_udp", :fields => {
+          "idgm/s"    => { :col_name => "idgmps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odgm/s"    => { :col_name => "odgmps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "noport/s"  => { :col_name => "noportps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idgmerr/s" => { :col_name => "idgmerrps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_sock6", :fields => {
+          "tcp6sck"  => { :col_name => "tcp6sck" , :data_type => :long },
+          "udp6sck"  => { :col_name => "udp6sck" , :data_type => :long },
+          "raw6sck"  => { :col_name => "raw6sck" , :data_type => :long },
+          "ip6-frag" => { :col_name => "ip6_frag", :data_type => :long },
+        }
+      },
+      {
+        :type => "net_ip6", :fields => {
+          "irec6/s"   => { :col_name => "irec6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fwddgm6/s" => { :col_name => "fwddgm6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idel6/s"   => { :col_name => "idel6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "orq6/s"    => { :col_name => "orq6ps"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmrq6/s"  => { :col_name => "asmrq6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmok6/s"  => { :col_name => "asmok6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "imcpck6/s" => { :col_name => "imcpck6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "omcpck6/s" => { :col_name => "omcpck6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragok6/s" => { :col_name => "fragok6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragcr6/s" => { :col_name => "fragcr6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_eip6", :fields => {
+          "ihdrer6/s" => { :col_name => "idrer6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iadrer6/s" => { :col_name => "iadrer6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iukwnp6/s" => { :col_name => "iukwnp6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "i2big6/s"  => { :col_name => "i2big6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idisc6/s"  => { :col_name => "idisc6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odisc6/s"  => { :col_name => "odisc6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "inort6/s"  => { :col_name => "inort6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "onort6/s"  => { :col_name => "onort6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "asmf6/s"   => { :col_name => "asmf6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fragf6/s"  => { :col_name => "fragf6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "itrpck6/s" => { :col_name => "itrpck6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_icmp6", :fields => {
+          "imsg6/s"   => { :col_name => "imsg6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "omsg6/s"   => { :col_name => "omsg6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iech6/s"   => { :col_name => "iech6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iechr6/s"  => { :col_name => "iechr6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oechr6/s"  => { :col_name => "oechr6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "igmbq6/s"  => { :col_name => "igmbq6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "igmbr6/s"  => { :col_name => "igmbr6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "ogmbr6/s"  => { :col_name => "ogmbr6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "igmbrd6/s" => { :col_name => "igmbrd6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "ogmbrd6/s" => { :col_name => "ogmbrd6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "irtsol6/s" => { :col_name => "irtsol6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "ortsol6/s" => { :col_name => "ortsol6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "irtad6/s"  => { :col_name => "irtad6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "inbsol6/s" => { :col_name => "inbsol6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "onbsol6/s" => { :col_name => "onbsol6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "inbad6/s"  => { :col_name => "inbad6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "onbad6/s"  => { :col_name => "onbad6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_eicmp6", :fields => {
+          "ierr6/s"   => { :col_name => "ierr6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idtunr6/s" => { :col_name => "idtunr6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odtunr6/s" => { :col_name => "odtunr6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "itmex6/s"  => { :col_name => "itmex6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "otmex6/s"  => { :col_name => "otmex6ps" , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iprmpb6/s" => { :col_name => "iprmpb6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oprmpb6/s" => { :col_name => "oprmpb6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "iredir6/s" => { :col_name => "iredir6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "oredir6/s" => { :col_name => "oredir6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "ipck2b6/s" => { :col_name => "ipck2b6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "opck2b6/s" => { :col_name => "opck2b6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_udp6", :fields => {
+          "idgm6/s"   => { :col_name => "idgm6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "odgm6/s"   => { :col_name => "odgm6ps"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "noport6/s" => { :col_name => "noport6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "idgmer6/s" => { :col_name => "idgmer6ps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "pwr_freq", :dev_name => "cpu_no", :fields => {
+          "MHz" => { :col_name => "mhz", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "pwr_fan", :dev_name => "fan_no", :fields => {
+          "rpm"    => { :col_name => "rpm"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "drpm"   => { :col_name => "drpm"  , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "DEVICE" => { :col_name => "device", :data_type => :string },
+        }
+      },
+      {
+        :type => "pwr_temp", :dev_name => "temp_no", :fields => {
+          "degC"   => { :col_name => "degc"    , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "%temp"  => { :col_name => "per_temp", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "DEVICE" => { :col_name => "device"  , :data_type => :string },
+        }
+      },
+      {
+        :type => "pwr_in", :dev_name => "in_no", :fields => {
+          "inV"    => { :col_name => "inv"   , :data_type => :numeric, :precision => 8, :scale => 2 },
+          "%in"    => { :col_name => "per_in", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "DEVICE" => { :col_name => "device", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "huge", :fields => {
+          "kbhugfree" => { :col_name => "kbhugfree"  , :data_type => :long },
+          "kbhugused" => { :col_name => "kbhugused"  , :data_type => :long },
+          "%hugused"  => { :col_name => "per_hugused", :data_type => :numeric, :precision => 5, :scale => 2 },
+        }
+      },
+      {
+        :type => "pwr_wghfreq", :dev_name => "cpu_no", :fields => {
+          "wghMHz" => { :col_name => "wghmhz", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
+      {
+        :type => "pwr_usb", :dev_name => "bus_no", :fields => {
+          "idvendor" => { :col_name => "idvendor", :data_type => :string },
+          "idprod"   => { :col_name => "idprod"  , :data_type => :string },
+          "maxpower" => { :col_name => "maxpower", :data_type => :long },
+          "manufact" => { :col_name => "manufact", :data_type => :string },
+          "product"  => { :col_name => "product" , :data_type => :string },
+        }
+      },
+      {
+        :type => "filesystem", :dev_name => "mount", :fields => {
+          "MBfsfree" => { :col_name => "mbfsfree"   , :data_type => :long },
+          "MBfsused" => { :col_name => "mbfsused"   , :data_type => :long },
+          "%fsused"  => { :col_name => "per_fsused" , :data_type => :numeric, :precision => 5, :scale => 2 },
+          "%ufsused" => { :col_name => "per_ufsused", :data_type => :numeric, :precision => 5, :scale => 2 },
+          "Ifree"    => { :col_name => "ifree"      , :data_type => :long },
+          "Iused"    => { :col_name => "iused"      , :data_type => :long },
+          "%Iused"   => { :col_name => "per_iused"  , :data_type => :numeric, :precision => 5, :scale => 2 },
+        }
+      },
+      {
+        :type => "net_fc", :dev_name => "fchost", :fields => {
+          "fch_rxf/s" => { :col_name => "fch_rxfps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fch_txf/s" => { :col_name => "fch_txfps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fch_rwx/s" => { :col_name => "fch_rwxps", :data_type => :numeric, :precision => 8, :scale => 2 },
+          "fch_txw/s" => { :col_name => "fch_twxps", :data_type => :numeric, :precision => 8, :scale => 2 },
+        }
+      },
     ]
 
     INDEX_HOSTNAME = 0
@@ -223,6 +453,13 @@ module SardataImporter
         case @type
         when "io" then (device_name == "-")
         when "disk" then (device_name != "-")
+        else false
+        end
+      when "DEVICE"
+        case @type
+        when "pwr_fan" then (device_name.match(/fan/))
+        when "pwr_temp" then (device_name.match(/temp/))
+        when "pwr_in" then (device_name.match(/in/))
         else false
         end
       else
@@ -284,6 +521,9 @@ module SardataImporter
         when dev_name.nil? then nil
         when dev_name == "cpu_no" then value.gsub(/cpu/, "")
         when dev_name == "irq_no" then value.gsub(/i00/, "").gsub(/i0/, "").gsub(/i/, "")
+        when dev_name == "fan_no" then value.gsub(/fan/, "")
+        when dev_name == "temp_no" then value.gsub(/temp/, "")
+        when dev_name == "in_no" then value.gsub(/in/, "")
         else value
         end
       end
